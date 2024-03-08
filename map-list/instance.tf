70f6e27296ead7623ca8a9e878e0a1f5a0429f14
NAGIREDDYNANDYALA <118894739+NAGIREDDYNANDYALA@users.noreply.github.com>
2024-03-08 14:50:53 +0530

    Delete map-list directory

--- map-list/instance.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "myname" {
-  ami           = lookup(var.ami, var.AWS_REGION)
-  instance_type = "t2.micro"
-  count         = 2
-  tags = {
-    Name = "nagireddy-${count.index}"
-  }
-}
